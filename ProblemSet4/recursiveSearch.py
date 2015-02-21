1e970118ca59dd0c9f3f53ea2f7f0516ff14b7db
fabbuki <fabbuki@gmail.com>
2015-02-20 22:12:31 -0800

    Did more hw

--- ProblemSet4/recursiveSearch.py
+++ ProblemSet4/recursiveSearch.py
@@ -31,39 +31,67 @@ delta = [[-1, 0], # go up
 
 delta_name = ['^', '<', 'v', '>']
 
+blocked = []
+g = 0
 
+current_g = []
 
-def search(grid,init,goal,cost):
-
-    init.append(0) #[0, 0, 0]
-    blocked = [init[-2:]] #get coordinates of blocked spot
-
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 1:
-                #add to blocked
-                blocked.append([i,j])
-
-    print blocked
-
-    current_spot = init[-2:]
-    g = 0
-
-    #check 4 directions
+def expand_search(current_spot, g):
+       #check 4 directions
     for direction in delta:
         spot = current_spot
         new_spot = [current_spot[0] + direction[0], current_spot[1] + direction[1]]
         print new_spot
+
+        #if we have the goal, end everything
+        if new_spot == goal:
+            print [g] + new_spot
+            return [g] + new_spot
+
         if ((new_spot[0] < 0) | (new_spot[0] > len(grid[0]))) | ((new_spot[1] < 0) | (new_spot[1] > len(grid))):
             #item is out of bounds; ignore it
             print "out of bounds"
-            print new_spot
         else:
             if new_spot in blocked:
                 print "skip" #if the new spot is in the blocked list, ignore it
-                print new_spot
             else:
                 print "we are in business"
+
+                blocked.append(new_spot) #add the newspot to the blocked list
+                expand_search(new_spot, g+1)
+
+init.append(0) #[0, 0, 0]
+blocked = [init[-2:]] #get coordinates of blocked spot
+
+#pour over grids to see where the blocked cells are to add it to our "blocked" list
+for i in range(len(grid)):
+    for j in range(len(grid[0])):
+        if grid[i][j] == 1:
+            #add to blocked
+            blocked.append([i,j])
+
+print blocked
+
+def search(grid,init,goal,cost):
+
+    current_spot = init[-2:]
+    g = 0
+
+    path = expand_search(current_spot, g)
+
+    # #check 4 directions
+    # for direction in delta:
+    #     spot = current_spot
+    #     new_spot = [current_spot[0] + direction[0], current_spot[1] + direction[1]]
+    #     print new_spot
+    #     if ((new_spot[0] < 0) | (new_spot[0] > len(grid[0]))) | ((new_spot[1] < 0) | (new_spot[1] > len(grid))):
+    #         #item is out of bounds; ignore it
+    #         print "out of bounds"
+    #     else:
+    #         if new_spot in blocked:
+    #             print "skip" #if the new spot is in the blocked list, ignore it
+    #         else:
+    #             print "we are in business"
 
 
 
@@ -74,9 +102,8 @@ def search(grid,init,goal,cost):
     #perform search (check if it is the GOAL)
     #generate search agents for each element with lowest g value
 
-    path = [11, 4, 5]
-
-
+    print "yolswag"
+    print path
 
     return path
 
