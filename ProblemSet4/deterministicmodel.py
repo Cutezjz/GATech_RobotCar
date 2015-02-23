d27c6c7cf9eedb54e326474d3d5d851c056af8bf
fabbuki <fabbuki@gmail.com>
2015-02-22 22:23:48 -0800

    I have basic g value reporting correctly

--- ProblemSet4/deterministicmodel.py
+++ ProblemSet4/deterministicmodel.py
@@ -34,11 +34,59 @@ delta_name = ['^', '<', 'v', '>'] # Use these when creating your policy grid.
 #  Modify the function stochastic_value below
 # ---------------------------------------------
 
+
+def solve_at_coordinate(grid, init, goal, cost):
+    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when search is complete
+    resign = False # flag set if we can't find expand
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return 1000
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            closed[x2][y2] = 1
+    return next[0] #this is the g value of the final
+
 def stochastic_value(grid,goal,cost_step,collision_cost,success_prob):
     failure_prob = (1.0 - success_prob)/2.0 # Probability(stepping left) = prob(stepping right) = failure_prob
     value = [[1000 for row in range(len(grid[0]))] for col in range(len(grid))]
     policy = [[' ' for row in range(len(grid[0]))] for col in range(len(grid))]
 
+    #solve for value
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                value[i][j] = 1000
+            else:
+                value[i][j] = solve_at_coordinate(grid, [i, j], goal, cost_step)
+
+    policy = []
     return value, policy
 
 # ---------------------------------------------
